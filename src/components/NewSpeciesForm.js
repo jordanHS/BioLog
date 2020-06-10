43f9042c5dc8d9e6203d2d4533a044d2b59c06f1
Jordan Safford <jordansafford@gmail.com>
2020-06-10 13:59:27 -0700

    build form component

--- src/components/NewSpeciesForm.js
+++ src/components/NewSpeciesForm.js
@@ -1,12 +1,43 @@
-// import React from "react";
-// import { v4 } from "uuid";
-// import PropTypes from "prop-types";
+import React from "react";
+import { v4 } from "uuid";
+import PropTypes from "prop-types";
 
 
-// function NewSpeciesForm(props) {
-//     return(
-//         <React.Fragment>
-//             <form onSubmit={handleNewSpeciesFormSubmission}
-//         </React.Fragment>
-//     )
-// }
+function NewSpeciesForm(props) {
+    return(
+        <React.Fragment>
+            <form onSubmit={handleNewSpeciesFormSubmission}>
+                <input
+                    type='text'
+                    name='commonName'
+                    placeholder='Common Name' />
+                <input
+                    type='text'
+                    name='sciName'
+                    placeholder='Scientific Name'/>
+                <input
+                    type='number'
+                    name='numberSeen'
+                    placeholder='0'/>
+                <textarea
+                    name='description'
+                    placeholder='Description'/>
+                <textarea
+                    name='notes'
+                    placeholder='Additional notes'/>
+                <button type='submit'>Submit</button>
+            </form>
+        </React.Fragment>
+    );
+
+    function handleNewSpeciesFormSubmission(event) {
+        event.preventDefault();
+        props.onNewSpeciesCreation({commonName: event.target.commonName.value, sciName: event.target.sciName.value, numberSeen: event.target.numberSeen.value, description: event.target.description.value, notes: event.target.notes.value})
+    }
+}
+
+NewSpeciesForm.propTypes = {
+    onNewSpeciesCreation: PropTypes.func
+}
+
+export default NewSpeciesForm;
