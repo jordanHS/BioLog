db5f215507bbc2164d9ca5938a049ac0126abba9
Jordan Safford <jordansafford@gmail.com>
2020-06-10 09:06:00 -0700

    build species component

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "./Header";
+import SpeciesControl from "./SpeciesControl";
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      {/* SpeciesList /> */}
+      <SpeciesControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Species.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Species(props) {
+    const { species } = props;
+    return (
+        <React.Fragment>
+            <div onClick = {() => props.whenSpeciesClicked(species.id)}>
+                <h2>Common name: {props.commonName}</h2>
+                <h3>Scientific Name: {props.sciName}</h3>
+            </div>
+        </React.Fragment>
+    );
+}
+
+Species.propTypes = {
+    commonName: propTypes.string,
+    sciName: propTypes.string,
+}
+
+export default Species;
+
