9de350469538d43fe3592a33732ab0246dc3f90f
Jordan Safford <jordansafford@gmail.com>
2020-06-18 16:14:51 -0700

    add router

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import Header from "./Header";
 import SpeciesControl from "./SpeciesControl";
+import Signin from "./Signin";
+import { Browser as Router, Switch, Route} from "react-router-dom;"
 
 
 function App() {
   return (
-    <React.Fragment>
-      <Header />
-      {/* SpeciesList /> */}
-      <SpeciesControl />
-    </React.Fragment>
+   <Router>
+     <Header />
+     <Switch>
+       <Route path="/signin">
+         <Signin />
+       </Route>
+       <Route path="/"> 
+          <SpeciesControl />
+       </Route>
+     </Switch>
+   </Router>
   );
 }
 
