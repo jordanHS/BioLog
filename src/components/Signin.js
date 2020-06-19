3cf166e5514ecbbe9caced24842121ef3328adc1
Jordan Safford <jordansafford@gmail.com>
2020-06-18 19:07:53 -0700

    add log-in function & form

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -10,9 +10,19 @@ function Signin(){
             console.log("registration successful!");
         }).catch(function(error) {
             console.log(error.message);
-        })
-    };
-};
+        });
+    }
+
+    function logIn(event) {
+        event.preventDefault();
+        const email = event.target.signinEmail.value;
+        const password = event.target.signPassword.value;
+        firebase.auth().signInWithEmailAndPassword(email, password).then(function(){
+            console.log("log-in successful!");
+        }).catch(function(error) {
+            console.log(error.message)
+        });
+    }
 
 return (
     <React.Fragment>
@@ -23,11 +33,24 @@ return (
                 name='email'
                 placeholder='email' />
             <input
-            type='password'
-            name='password'
-            placeholder='Password'/>    
+                type='password'
+                name='password'
+                placeholder='Password'/> 
+            <button type="submit">Register</button>
+        </form>
+        <h1>Log-in</h1>
+        <form onSubmit={logIn}>
+            <input
+                type='text'
+                name='signinEmail'
+                placeholder='email'/>
+            <input
+                type='password'
+                name='signinPassword'
+                placeholder='Password'/>
+            <button type='submit'>Log in</button>    
         </form>
     </React.Fragment>
 )
-
+}
 export default Signin
